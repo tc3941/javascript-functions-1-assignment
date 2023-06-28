a118bfeaa9f748721b8dfbfe6bb002fd63713dd2
Teddy Conyers Jr <tc3941@rit.edu>
2023-06-27 21:10:26 -0400

    Add changes as directed in the exercises

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 //Define a function underneath here that is named numberAdder
 //numberAdder should take 2 parameters and it should return the sum of those two values
-//After you're done, try looking around at the CSS and HTML to 
+//After you're done, try looking around at the CSS and HTML to
 //see if you understand what's going on :)
-
+function numberAdder(num1, num2) {
+  return num1 + num2
+}
